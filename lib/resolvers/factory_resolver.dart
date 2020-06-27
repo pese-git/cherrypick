29b158a031d20bfd1b79e55f3736891953ec424d
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2020-06-27 16:47:20 +0300

    fixed factory resolver

--- lib/resolvers/factory_resolver.dart
+++ lib/resolvers/factory_resolver.dart
@@ -4,7 +4,7 @@ import 'package:dart_di/resolvers/resolver.dart';
  * Разрешает зависимость для фабричной функции
  */
 class FactoryResolver<T> extends Resolver<T> {
-  Function _factory;
+  final T Function() _factory;
 
   FactoryResolver(this._factory);
 
