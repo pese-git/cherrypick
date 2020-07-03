3a9d182b84edbc8b830409732276cbd532995b36
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2020-07-03 10:00:06 +0300

    added exports for library

---
+++ lib/dart_di.dart
@@ -0,0 +1,5 @@
+library dart_di;
+
+export 'package:dart_di/di_container.dart';
+export 'package:dart_di/resolvers/resolvers.dart';
+export 'package:dart_di/resolvers/resolving_context.dart';
---
+++ lib/resolvers/resolvers.dart
@@ -0,0 +1,7 @@
+/// Resolvers определяет, как разрешить зависимость для контейнера
+library resolvers;
+
+export 'resolver.dart';
+export 'factory_resolver.dart';
+export 'value_resolver.dart';
+export 'singelton_resolver.dart';
