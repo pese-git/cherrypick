e102b15022d73d04e265b468cc57171b88e36c9e
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2021-04-22 15:22:09 +0300

    added documents

--- lib/experimental/di.dart
+++ lib/experimental/di.dart
@@ -1,1 +1,26 @@
-class DartDi {}
+import 'package:dart_di/experimental/scope.dart';
+
+Scope? _rootScope = null;
+
+class DartDi {
+  /// RU: Метод открывает главный [Scope].
+  /// ENG: The method opens the main [Scope].
+  ///
+  /// return
+  static Scope openRootScope() {
+    if (_rootScope == null) {
+      _rootScope = Scope(null);
+    }
+    return _rootScope!;
+  }
+
+  /// RU: Метод закрывает главный [Scope].
+  /// ENG: The method close the main [Scope].
+  ///
+  ///
+  static void closeRootScope() {
+    if (_rootScope != null) {
+      _rootScope = null;
+    }
+  }
+}
