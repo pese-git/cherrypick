8fd18df811061cb6bf1ea95f35a5f756a1dcc742
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-07-29 17:16:22 +0300

    feat: enable CherryPick cycle detection in debug mode and use safe root scope

--- examples/postly/lib/main.dart
+++ examples/postly/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:cherrypick/cherrypick.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:postly/app.dart';
 import 'di/app_module.dart';
 
 void main() {
+  // Включаем cycle-detection только в debug/test
+  if (kDebugMode) {
+    CherryPick.enableGlobalCycleDetection();
+    CherryPick.enableGlobalCrossScopeCycleDetection();
+  }
+
+  // Используем safe root scope для гарантии защиты
   CherryPick.openRootScope().installModules([$AppModule()]);
   runApp(MyApp());
 }
