8863b10cbe2b9d130c30f8a0fa735eaa63a5354d
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2025-08-14 16:57:25 +0300

    feat: update dns

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cherrypick-di.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
